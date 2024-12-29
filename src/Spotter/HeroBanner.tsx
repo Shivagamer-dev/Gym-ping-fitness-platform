c35fabcdcf5a341242671958a7a4b3c991b9afa1
Amil Lal <amil.lal2003@gmail.com>
2024-12-29 17:11:33 +0530

    Hero Banner Tagline changes

--- src/Spotter/HeroBanner.tsx
+++ src/Spotter/HeroBanner.tsx
@@ -5,10 +5,10 @@ import HeroBannerImage from '../assets/images/banner.png'
 const HeroBanner = () => {
   return (
     <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography color="#FF2625" fontWeight="800" fontSize="80px">Gym-Ping</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
+      Find! Fit! <br />
+      Thrive!
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
       Check out the most effective exercises personalized to you
